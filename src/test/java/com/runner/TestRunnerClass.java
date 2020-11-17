555e71a3dc46d2d7e174344a4df007d6d4857e1b
idris22k <idris22k@gmail.com>
2020-11-17 17:30:36 +0530

    msg

--- src/test/java/com/runner/TestRunnerClass.java
+++ src/test/java/com/runner/TestRunnerClass.java
@@ -3,12 +3,13 @@ package com.runner;
 import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
 import cucumber.api.junit.Cucumber;
 
 
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src\\test\\resources",glue= {"com.stepdefination"})
+@CucumberOptions(features = "src\\test\\resources",glue= {"com.stepdefination"}, monochrome = true, snippets = SnippetType.CAMELCASE, strict = true, plugin = { "pretty", "html:src\\test\\ressources\\reports"})
 public class TestRunnerClass {
 
 
